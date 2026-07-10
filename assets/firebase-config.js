5503abc9ac10e9c08cca6e7d4d5dfc36f353c8f3
Naufil7706 <skillsphere.work@gmail.com>
2026-07-11 03:14:36 +0530

    New Vacancies Files

---
+++ assets/firebase-config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyBuf3fGQoGeOHHjJutYUKj-0rF-ZgY1--E",
+    authDomain: "sse-vacancies.firebaseapp.com",
+    projectId: "sse-vacancies",
+    storageBucket: "sse-vacancies.firebasestorage.app",
+    messagingSenderId: "683166843651",
+    appId: "1:683166843651:web:07e92f767b41713630df36"
+  };
